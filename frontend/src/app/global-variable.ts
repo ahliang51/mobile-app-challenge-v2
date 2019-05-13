5e813716ba81590bc35cbc95ad8e7c0c43c1c402
shengliang <ahliang51@hotmail.com>
2019-05-13 15:16:45 +0800

    added splash images

--- frontend/src/app/global-variable.ts
+++ frontend/src/app/global-variable.ts
@@ -1,3 +1,4 @@
 // export const apiUrl = 'http://localhost:3000';
 // export const apiUrl = 'http://192.168.1.77:3000';
-export const apiUrl = 'http://192.168.43.200:3100';
+// export const apiUrl = 'http://192.168.43.200:3100';
+export var apiUrl: string = 'http://ec2-18-191-133-237.us-east-2.compute.amazonaws.com:3100'
